f4067ba7f0af2b5576537e58d3b40811d274aeeb
brenbtc <yanohb@yahoo.com>
2023-03-31 22:54:17 -0600

    Styling on rendertitle

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,9 @@ const RenderCards = ({data, title}) => {
   return data.map((post) => <Card key={post._id} {...post} />)
   }
   return (
-    <h2>{title}</h2>
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
+      {title}
+    </h2>
   )
 }
 
