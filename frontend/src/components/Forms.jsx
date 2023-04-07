c02092afb736353d513231ac101357dd4e3f1adb
mhoong <mhoong2@yahoo.com>
2023-04-06 21:47:28 -0600

    continued development of Potulate.jsx and created froms in components/Forms.jsx

--- frontend/src/components/Forms.jsx
+++ frontend/src/components/Forms.jsx
@@ -1,9 +1,38 @@
-const Forms = () => {
+const Forms = ({ labelName, type, name, placeholder,value, handleChange, isRandPrompt, handleRandPrompt }) => {
     return (
       <div>
-        <form>
+        {/* <form>
           <input />
-        </form>
+        </form> */}
+        <div>
+          <div className="flex items-center gap-2 mb-2">
+            <label
+              htmlFor={name}
+              className='block text-sm font-medium text-black'
+            >
+              {labelName}
+            </label>
+            {isRandPrompt && (
+              <button
+                type='button'
+                onClick={handleRandPrompt}
+                className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+              >
+                Randomize
+              </button>
+            )}
+          </div>
+          <input
+            type={type}
+            id={name}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            onChange={handleChange}
+            required
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+          />
+        </div>
       </div>
     )
   }
